a5bcb2b92a7c27eee7bbd37b77ade75380432732
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-25 23:29:20 +0430

    create elements dinamically

--- src/App.js
+++ src/App.js
@@ -3,26 +3,39 @@ import './App.css'
 
 function App() {
 
-  const blogObj = {
-    title: 'Blog Title 1',
-    description: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor '
-  } 
+  const blogArray = [
+    {
+      id: 1,
+      title: 'Blog Title 1',
+      description: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor '
+    },
+    {
+      id: 2,
+      title: 'Blog Title 2',
+      description: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor '
+    },
+    {
+      id: 3,
+      title: 'Blog Title 3',
+      description: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor '
+    }
+  ] 
+
+  const blogCards = blogArray.map((item, pos) => {
+    console.log(item)
+
+    return (
+      <div className="BlogCard" key={item.id}>
+         <h3>{item.title}</h3>
+         <p>{item.description}</p> 
+      </div> 
+    )
+
+  })
 
   return (
     <div className="App">
-      <div className="BlogCard">
-        <h3>{blogObj.title}</h3>
-        <p>{blogObj.description}</p> 
-      </div>
-      <div className="BlogCard">
-        <h3>{blogObj.title}</h3>
-        <p>{blogObj.description}</p> 
-      </div>
-      <div className="BlogCard">
-        <h3>{blogObj.title}</h3>
-        <p>{blogObj.description}</p> 
-      </div> 
-
+      {blogCards}
     </div>
   );
 }
