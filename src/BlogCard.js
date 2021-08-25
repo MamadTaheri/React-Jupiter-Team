dc823591d4300fd18a0e88bae8313087a3404b0b
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-26 00:14:52 +0430

    Add CSS Mpdules to project

--- src/BlogCard.js
+++ src/BlogCard.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 import { dumpLogs } from './Utils'
 
+// import './BlogCard.css'
+import classes from './BlogCard.module.css'
+
 const BlogCard = (props) => {
     dumpLogs(props.title)
 
     return (        
-        <div className="BlogCard">
+        <div className={classes.NewBlogCard}>
            <h3>{props.title}</h3>
            <p>{props.description}</p> 
         </div> 
