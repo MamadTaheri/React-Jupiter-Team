a95065c3b7f12ec9c861c738ff37992eca1f344a
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-26 02:22:57 +0430

    updateing project

--- src/App.js
+++ src/App.js
@@ -1,88 +1,28 @@
 import React, { Component } from 'react'
 
-import BlogHomePageCard from './BlogCard'
-import { isArrayEmpty as isMyArrayEmpty } from './Utils'
+import classes from './App.module.css'
+import ProductData from './ProductData';
 
-import './App.css'
-
-class App extends React.Component {
-  state = {
-    showBlogs: true,
-    blogArray: [
-      {
-        id: 1,
-        title: 'Blog Title 1',
-        description: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor ',
-        likeCount: 0,
-      },
-      {
-        id: 2,
-        title: 'Blog Title 2',
-        description: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor ',
-        likeCount: 0,
-      },
-      {
-        id: 3,
-        title: 'Blog Title 3',
-        description: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor ',
-        likeCount: 0,
-      }
-    ]  
-  } 
-
-  onLikeBtnClick = (pos) => {
-    const updatedBlogList = this.state.blogArray
-    const updatedBlogObj = updatedBlogList[pos]
-
-    updatedBlogObj.likeCount = updatedBlogObj.likeCount + 1
-    updatedBlogList[pos] = updatedBlogObj
-
-    this.setState({blogArray: updatedBlogList})
-
-    // console.log(updatedBlogObj)
-  } 
-
-   onHideBtnClick = () => {
-    //  let updatedState = !this.state.showBlogs
-    this.setState((prevState, prevProps) => {
-      return {showBlogs: !prevState.showBlogs}
-    })
-
-    console.log(this.state.showBlogs)
-  }
-
-  render() {
-    console.log('Render')
-
-    const blogCards = isMyArrayEmpty(this.state.blogArray) ? [] : this.state.blogArray.map((item, pos) => {
-
-      return (
-        <BlogHomePageCard 
-          key={pos} 
-          title={item.title} 
-          description={item.description}
-          id={item.id} 
-          likeCount={item.likeCount}
-          onLikeBtnClick={() => this.onLikeBtnClick(pos)}
-           
-        />
-        // <div className="BlogCard" key={item.id}>
-        //    <h3>{item.title}</h3>
-        //    <p>{item.description}</p> 
-        // </div> 
-      ) 
-    })
-
+function App() {  
     return(
       <div className="App">
-      <button onClick ={this.onHideBtnClick}>{this.state.showBlogs ? 'Hide List' : 'Show List'}</button>
-      <br/>
-      {
-        this.state.showBlogs ? blogCards : null
-      }
-    </div>
-    )
-  } 
+        <header className="App-header">
+          <nav className={classes.Topbar}>
+            <img src="https://logos-download.com/wp-content/uploads/2016/03/Amazon_Logo_2000.png" alt="Amazon Logo" />
+          </nav>
+        </header>
+
+        <div className={classes.MainContainer}>
+          <div className={classes.ProductPreview}>
+            <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview" />
+          </div>
+
+          <div className={classes.ProductData}>
+            <h1>{ProductData.title}</h1>
+          </div>
+        </div>
+      </div>
+    ) 
 }
 
 export default App;
