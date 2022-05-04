432cc481ff9f247e8d289740dfc126941794e6ea
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-05 00:23:01 +0430

    handle isActive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,9 @@ import "../styles/Card.css";
 import ToggleSwitch from "./ToggleSwitch";
 
 const Card = ({ person }) => {
-  const { id, fullName, jobTitle, isActive = true } = person;
+  const { id, fullName, jobTitle } = person;
+  const isActive = Math.floor(Math.random() * 10) % 2 === 0;
+
   return (
     <div className={isActive ? "container-card" : "container-card-inactive"}>
       <div className="header-card">
