5dc73a67e460eae306b54acbad664fdf1711194c
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-05 00:17:54 +0430

    handle HomeView

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,30 +1,22 @@
-import { Link } from 'react-router-dom';
-import '../styles/Card.css';
-import ToggleSwitch from './ToggleSwitch';
+import { Link } from "react-router-dom";
+import "../styles/Card.css";
+import ToggleSwitch from "./ToggleSwitch";
 
-function Card({ text, title, onPress = () => { }, isActive = true }) {
-    return (
-        <Link to={`/cards/${title}`}> <div onClick={onPress} className={isActive ? 'container-card' : 'container-card-inactive'}>
-            <div className='header-card'>
-                <label className='title-card'>{title}</label>
-                <ToggleSwitch />
-            </div>
-            <label className='text-card'>
-                {text.length <= 102 ? text : `${text.substring(0, 102)} ...`}
-            </label>
-        </div>
-        </Link>
-    );
-}
+const Card = ({ person }) => {
+  const { id, fullName, jobTitle, isActive = true } = person;
+  return (
+    <div className={isActive ? "container-card" : "container-card-inactive"}>
+      <div className="header-card">
+        <label className="title-card">{fullName}</label>
+        <ToggleSwitch />
+      </div>
+      <label className="text-card">
+        {jobTitle.length <= 102
+          ? jobTitle
+          : `${jobTitle.substring(0, 102)} ...`}
+      </label>
+    </div>
+  );
+};
 
 export default Card;
-
-{/* <div className="card" style={{ width: "18rem" }}>
-    <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="card-link">Card link</a>
-    <a href="#" className="card-link">Another link</a>
-    </div>
-</div> */}
